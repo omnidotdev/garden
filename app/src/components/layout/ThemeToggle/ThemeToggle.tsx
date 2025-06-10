8ef891e4512bcc64ebef70ba2ddd3168934806b8
Brian Cooper <brian@brian-cooper.com>
2025-06-10 16:31:45 -0500

    style(theme-toggle): set ghost variant

--- app/src/components/layout/ThemeToggle/ThemeToggle.tsx
+++ app/src/components/layout/ThemeToggle/ThemeToggle.tsx
@@ -21,12 +21,13 @@ const ThemeToggle = () => {
 
   return (
     <Button
-      variant="outline"
+      variant="ghost"
       size="icon"
       onClick={toggleTheme}
       title="Toggle theme"
     >
       <Icons.Sun className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
+
       <Icons.Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
