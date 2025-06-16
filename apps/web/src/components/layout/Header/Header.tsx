254079860b5171e0e43ed551136a7e0daeaa73f9
Brian Cooper <brian@brian-cooper.com>
2025-06-16 12:41:52 -0500

    feature: add logo

--- apps/web/src/components/layout/Header/Header.tsx
+++ apps/web/src/components/layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SproutIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 import { ThemeToggle } from "@/components/layout";
@@ -11,7 +12,8 @@ import { ThemeToggle } from "@/components/layout";
 const Header = () => (
   <header className="sticky top-0 z-50 flex w-full items-center justify-between border-b bg-background/30 p-4 backdrop-blur-lg">
     <Link href="/" className="flex items-center gap-2">
-      <SproutIcon className="h-6 w-6 text-primary" />
+      <Image src="/logo.png" width={30} height={20} alt="Logo" />
+
       <h1 className="font-bold text-xl">Garden</h1>
     </Link>
 
