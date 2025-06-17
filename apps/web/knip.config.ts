f3fcd12f6e464bb2db468a3d1914a2227cdd703d
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-06-17 08:17:35 -0500

    refactor: isolate dependencies in apps

--- apps/web/knip.config.ts
+++ apps/web/knip.config.ts
@@ -8,13 +8,9 @@ const knipConfig: KnipConfig = {
   rules: {
     binaries: "off",
   },
-  // NB: Modified from the default Next.js configuration, see: https://knip.dev/reference/plugins/next
-  entry: [
-    "src/app/**/{error,layout,loading,not-found,page,template,default,route}.{ts,tsx}",
-  ],
   // NB: files are reported as unused if they are in the set of project files, but not in the set of files resolved from the entry files. See: https://knip.dev/guides/configuring-project-files
-  project: ["src/**/*.{ts,tsx}", "!src/scripts/**"],
-  ignore: ["src/generated/**"],
+  project: ["src/**/*.{ts,tsx}"],
+  ignoreDependencies: ["tailwindcss", "tw-animate-css"],
   // TODO: re-enabled. Broken currently with biome v2 and updated extends pattern
   biome: false,
 };
